df16e46cc15c739e8ec9bcdd5e04bccf56e42521
Jack <jacktgriffiths@protonmail.com>
2025-01-07 00:47:38 +0000

    Added resume

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t sb2nov/latex .
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex jack_griffiths_resume.tex
